929cf365368633fb2a6bb67262b55b08aea8f2e6
rae626 <116301158+rae626@users.noreply.github.com>
2023-03-14 23:16:40 -0400

    Set up the front end

---
+++ src/main/java/com/example/calculator/HelloApplication.java
@@ -0,0 +1,29 @@
+package com.example.calculator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 600, Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        //scene.setFill(Color.TRANSPARENT);
+        stage.setResizable(false);
+        stage.setFullScreen(false);
+        stage.setTitle("Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
